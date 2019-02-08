d4db581fabc58409fdc0cc6de1a41e7d53efb87b
JBeto <jbeto001@ucr.edu>
2019-02-08 10:39:14 -0800

    Haha

--- .Secret/Database/database.cpp
+++ .Secret/Database/database.cpp
@@ -5,7 +5,7 @@ std::string Database::match(const std::vector<std::string> &vs) {
     std::vector<std::string> gg = {"place", "Highland", "Park.", "park", "completely", "empty" , "Monday" ,"night." 
     "Omar", "walking" , "next", "swings" , "mysterious", "figure", "takes", "ground."};
     if (vs.size() != gg.size()) return "No Match";
-    for (int i = 0; i < gg.size(); i++) {
+    for (unsigned i = 0; i < gg.size(); i++) {
         if (vs.at(i) != gg.at(i)) return "No Match";
     }
     return "Andre Castro";
